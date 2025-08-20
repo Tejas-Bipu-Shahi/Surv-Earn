20f3e6e917a28c74a8eb0d38abd5c64000c7c5bb
Tejas-Bipu-Shahi <shahitejas899@gmail.com>
2025-08-20 23:11:00 +0545

    added otp generation function

---
+++ src/utils/otp_generator.py
@@ -0,0 +1,12 @@
+import string
+import secrets
+
+
+def generate_otp(length: int = 4) -> str:
+    characters = string.ascii_letters + string.digits
+    otp = ''.join(secrets.choice(characters) for _ in range(length))
+    return otp
+
+
+if __name__ == '__main__':
+    print(generate_otp())
