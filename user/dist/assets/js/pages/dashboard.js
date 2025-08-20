32da5cfd79630939bf9f8d3f58a85f3043f3a457
Nick_Zoc <naitikjoshi888@gmail.com>
2025-08-21 02:50:47 +0545

    Feature: modifed index.html and modified dashboard.js: major overhaul to dashboard content, introduced new cards. Minor changes: fillSurvey.html, redeem.html

--- user/dist/assets/js/pages/dashboard.js
+++ user/dist/assets/js/pages/dashboard.js
@@ -3,34 +3,34 @@ var optionsProfileVisit = {
 		position: 'back'
 	},
 	dataLabels: {
-		enabled:false
+		enabled: false
 	},
 	chart: {
 		type: 'bar',
 		height: 300
 	},
 	fill: {
-		opacity:1
+		opacity: 1
 	},
 	plotOptions: {
 	},
 	series: [{
-		name: 'sales',
-		data: [9,20,30,20,10,20,30,20,10,20,30,20]
+		name: 'surveys_completed',
+		data: [2, 3, 4, 3, 2, 4, 5, 4, 3, 4, 5, 6]
 	}],
 	colors: '#435ebe',
 	xaxis: {
-		categories: ["Jan","Feb","Mar","Apr","May","Jun","Jul", "Aug","Sep","Oct","Nov","Dec"],
+		categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
 	},
 }
-let optionsVisitorsProfile  = {
-	series: [70, 30],
-	labels: ['Male', 'Female'],
-	colors: ['#435ebe','#55c6e8'],
+let optionsVisitorsProfile = {
+	series: [76, 15, 9],
+	labels: ['Completed', 'Pending', 'Rejected'],
+	colors: ['#435ebe', '#55c6e8', '#dc3545'],
 	chart: {
 		type: 'donut',
 		width: '100%',
-		height:'350px'
+		height: '350px'
 	},
 	legend: {
 		position: 'bottom'
@@ -38,7 +38,7 @@ let optionsVisitorsProfile  = {
 	plotOptions: {
 		pie: {
 			donut: {
-				size: '30%'
+				size: '40%'
 			}
 		}
 	}
@@ -47,13 +47,13 @@ let optionsVisitorsProfile  = {
 var optionsEurope = {
 	series: [{
 		name: 'series1',
-		data: [310, 800, 600, 430, 540, 340, 605, 805,430, 540, 340, 605]
+		data: [30, 40, 55, 48, 60, 62, 70, 68, 72, 75, 78, 80]
 	}],
 	chart: {
 		height: 80,
 		type: 'area',
 		toolbar: {
-			show:false,
+			show: false,
 		},
 	},
 	colors: ['#5350e9'],
@@ -61,28 +61,28 @@ var optionsEurope = {
 		width: 2,
 	},
 	grid: {
-		show:false,
+		show: false,
 	},
 	dataLabels: {
 		enabled: false
 	},
 	xaxis: {
 		type: 'datetime',
-		categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z","2018-09-19T07:30:00.000Z","2018-09-19T08:30:00.000Z","2018-09-19T09:30:00.000Z","2018-09-19T10:30:00.000Z","2018-09-19T11:30:00.000Z"],
+		categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z", "2018-09-19T07:30:00.000Z", "2018-09-19T08:30:00.000Z", "2018-09-19T09:30:00.000Z", "2018-09-19T10:30:00.000Z", "2018-09-19T11:30:00.000Z"],
 		axisBorder: {
-			show:false
+			show: false
 		},
 		axisTicks: {
-			show:false
+			show: false
 		},
 		labels: {
-			show:false,
+			show: false,
 		}
 	},
-	show:false,
+	show: false,
 	yaxis: {
 		labels: {
-			show:false,
+			show: false,
 		},
 	},
 	tooltip: {
