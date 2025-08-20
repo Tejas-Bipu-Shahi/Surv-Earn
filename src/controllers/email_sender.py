182c6900af388c3d2a4027c5345dd8e81f7321a9
Tejas-Bipu-Shahi <shahitejas899@gmail.com>
2025-08-21 03:05:57 +0545

    added status return value for send_otp_mail

--- src/controllers/email_sender.py
+++ src/controllers/email_sender.py
@@ -1,16 +1,26 @@
 from flask_mail import Message
 
+# logging
+from icecream import ic
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from flask_mail import Mail
 
 
-def send_otp_mail(mail: 'Mail', otp: str, recipient: str, sender: str) -> None:
-    msg = Message(
-        subject='SurvEarn OTP Verification',
-        recipients=[recipient],
-        html=f'Your SurvEar Verification Code is: <b>{otp}</b>',
-        sender=f'SurvEarn Team <{sender}>'
-    )
-    mail.send(msg)
+def send_otp_mail(mail: 'Mail', otp: str, recipient: str, sender: str) -> tuple[bool, str]:
+    try:
+        msg = Message(
+            subject='SurvEarn OTP Verification',
+            recipients=[recipient],
+            html=f'Your SurvEar Verification Code is: <b>{otp}</b>',
+            sender=f'SurvEarn Team <{sender}>'
+        )
+        mail.send(msg)
+
+        ic(f'Email sent to {recipient}.')
+
+    except Exception as e:
+        return False, str(e)
+    return True, 'Success'
