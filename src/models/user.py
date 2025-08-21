a9e3cb979c0fe3b85f4265d229400e7e8a740e06
Tejas-Bipu-Shahi <shahitejas899@gmail.com>
2025-08-22 03:48:00 +0545

    added more info fields for user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
 from flask_login import UserMixin
+from datetime import date
+
+from models.survey import Survey
 
 
 class User(BaseModel, UserMixin):
@@ -7,6 +10,21 @@ class User(BaseModel, UserMixin):
     password_hash: str
     username: str
     _id: str | None = None
+
+    total_completed_surveys: int = 0
+    current_balance: int = 0
+    total_earnings: int = 0
+    completed_surveys: list[Survey] | None = None
+
+    full_name: str | None = None
+    phone_number: int | None = None
+    dob: date | None = None
+    address: str | None = None
+    zip_code: int | None = None
+    occupation: str | None = None
+    gender: str | None = None
+    city: str | None = None
+    education: str | None = None
 
     def get_id(self):
         return self.email
