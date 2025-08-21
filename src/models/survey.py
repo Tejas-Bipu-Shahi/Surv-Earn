d3b1314ee156d70d690d7814a2457af4bb5c69de
Tejas-Bipu-Shahi <shahitejas899@gmail.com>
2025-08-22 03:46:42 +0545

    added survey model

---
+++ src/models/survey.py
@@ -0,0 +1,24 @@
+from datetime import date
+from enum import StrEnum
+
+from pydantic import BaseModel
+
+
+class SurveyStatus(StrEnum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+    EXPIRED = "expired"
+
+
+class Survey(BaseModel):
+    survey_name: str
+    company_name: str
+    reward_per_completion: int
+    expiration_date: date
+    _id: str | None = None
+
+    status: SurveyStatus | None = None
+    estimated_time: int | None = None
+    date_posted: date | None = None
+    questions_count: int | None = None
+    category: str | None = None
